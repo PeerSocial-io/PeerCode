742c4ee92beaba143e8aadf409ab0f7a199f1cad
Bradley Matusiak <bmatusiak@gmail.com>
2022-07-01 14:52:50 +0000

    update

---
+++ setup.sh
@@ -0,0 +1,20 @@
+rm -rf ./node_modules
+rm -rf ./peersocial.io
+
+git clone https://github.com/PeerSocial-io/PeerSocial.io.git ./peersocial.io
+
+cd ./peersocial.io
+npm install
+
+cd ../
+mv ./peersocial.io/node_modules ./node_modules
+cd ./node_modules
+ln -s ../peersocial.io ./peersocial.io
+cd ../peersocial.io
+
+ln -s ../node_modules ./node_modules
+
+npm run build-dev
+
+# npm install ./peersocial.io
+
