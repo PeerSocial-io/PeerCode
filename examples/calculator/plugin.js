6c3ef01c2088e821954ff28a91dc75cdabe5bd98
Bradley Matusiak <bmatusiak@gmail.com>
2022-07-01 03:02:26 +0000

    update

--- examples/calculator/plugin.js
+++ examples/calculator/plugin.js
@@ -1,6 +1,6 @@
 define(function (require, exports, module) {
 
-  appPlugin.consumes = ["app", "plugins"];
+  appPlugin.consumes = ["app", "plugins", "state"];
   appPlugin.provides = ["calculator"];
 
   /* global $ */
@@ -8,9 +8,55 @@ define(function (require, exports, module) {
 
   function appPlugin(options, imports, register) {
 
-    var Plugin = imports.plugin;
+    var Plugin = imports.plugins.Plugin;
+    var app = imports.app;
+    var state = imports.state;
+
+    state.home = "/calculator";
 
     var calculator = new Plugin();
+
+    calculator.init = function () {
+      state.$hash.on("/calculator", function (args, currentState, lastState, onDestroy) {
+        //https://www.geeksforgeeks.org/html-calculator/
+        $("#main-container").html(require("text!./calculator.html"))
+        $("#main-container").find("input[value='c']").on("click", clr)
+        var disA = [
+          "1","2","3",
+          "4","5","6",
+          "7","8","9", 
+          "0","/","-",
+          "+",".","*"
+        ];
+        for (var i in disA) {
+          ((i) => {
+            $("#main-container").find("input[value='" + disA[i] + "']").on("click", function () {
+              dis(disA[i])
+            })
+          })(i);
+        }
+        $("#main-container").find("input[value='=']").on("click", function () {
+          solve()
+        })
+        //function that display value
+        function dis(val) {
+          $("#result").val( $("#result").val() + val )
+        }
+
+        //function that evaluates the digit and return result
+        function solve() {
+          let x = $("#result").val()
+          let y = eval(x)
+          $("#result").val(y)
+        }
+
+        //function that clear the display
+        function clr() {
+          $("#result").val("")
+        }
+
+      })
+    };
 
     register(null, {
       calculator: calculator
