c050430464bfb77ecd458c656f277c2989b36aa3
Bradley Matusiak <bmatusiak@gmail.com>
2022-07-17 13:54:59 -0400

    Update plugin.js

--- examples/calculator/plugin.js
+++ examples/calculator/plugin.js
@@ -14,43 +14,46 @@ function appPlugin(options, imports, register) {
   calculator.init = function () {
     state.$hash.on("/calculator", function (args, currentState, lastState, onDestroy) {
       //https://www.geeksforgeeks.org/html-calculator/
-      $("#main-container").html(require("text!./calculator.html"))
-      
-      $("#main-container").find("input[value='c']").on("click", clr)
-      var disA = [
-        "1","2","3",
-        "4","5","6",
-        "7","8","9", 
-        "0","/","-",
-        "+",".","*"
-      ];
-      for (var i in disA) {
-        ((i) => {
-          $("#main-container").find("input[value='" + disA[i] + "']").on("click", function () {
-            dis(disA[i])
-          })
-        })(i);
-      }
-      $("#main-container").find("input[value='=']").on("click", function () {
-        solve()
-      })
-      //function that display value
-      function dis(val) {
-        $("#result").val( $("#result").val() + val )
-      }
+      require(["./calculator.html"],function(calculator_html){
+        $("#main-container").html(calculator_html)
 
-      //function that evaluates the digit and return result
-      function solve() {
-        let x = $("#result").val()
-        let y = eval(x)
-        $("#result").val(y)
-      }
 
-      //function that clear the display
-      function clr() {
-        $("#result").val("")
-      }
+        $("#main-container").find("input[value='c']").on("click", clr)
+        var disA = [
+          "1","2","3",
+          "4","5","6",
+          "7","8","9", 
+          "0","/","-",
+          "+",".","*"
+        ];
+        for (var i in disA) {
+          ((i) => {
+            $("#main-container").find("input[value='" + disA[i] + "']").on("click", function () {
+              dis(disA[i])
+            })
+          })(i);
+        }
+        $("#main-container").find("input[value='=']").on("click", function () {
+          solve()
+        })
+        //function that display value
+        function dis(val) {
+          $("#result").val( $("#result").val() + val )
+        }
 
+        //function that evaluates the digit and return result
+        function solve() {
+          let x = $("#result").val()
+          let y = eval(x)
+          $("#result").val(y)
+        }
+
+        //function that clear the display
+        function clr() {
+          $("#result").val("")
+        }
+        
+      });
     });
     
     state.home = "/calculator";
